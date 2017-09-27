9681c3699721f3829f2f65e08793b0b026799eaa
Shijie Liao <374682617@qq.com>
2017-09-27 22:27:28 +0800

    maven settings.xml

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * 文件名：Test.java
+ * 版权：Copyright 2007-2017 Liao Shijie Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： Test.java
+ * 修改人：KOBE
+ * 修改时间：2017年9月27日
+ * 修改内容：新增
+ */
+package test;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * <pre>
+ * </pre>
+ * 
+ * @author     KOBE
+ */
+public class Test {
+
+}
