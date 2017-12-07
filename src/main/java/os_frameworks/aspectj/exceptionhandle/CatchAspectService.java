d8f7332d1cb01d9a11ac550c56613b85deac4f68
JateLiao <374682617@qq.com>
2017-12-07 19:11:59 +0800

    aspectj

---
+++ src/main/java/os_frameworks/aspectj/exceptionhandle/CatchAspectService.java
@@ -0,0 +1,33 @@
+/*
+ * 文件名：CatchAspectService.java
+ * 版权：Copyright 2007-2017 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： CatchAspectService.java
+ * 修改人：tianzhong
+ * 修改时间：2017年12月7日
+ * 修改内容：新增
+ */
+package os_frameworks.aspectj.exceptionhandle;
+
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * <pre>
+ * </pre>
+ * 
+ * @author     tianzhong
+ */
+@Aspect
+public class CatchAspectService {
+
+    @Around("execution(* com.lsj..*(..))")
+    public void catchMonitor(){
+        
+    }
+}
